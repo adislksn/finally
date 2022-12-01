94563896ac898970557dae69cb535a7107f0b7be
adislksn <adi.120140038@student.itera.ac.id>
2022-12-01 18:13:35 +0700

    Update Register

--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -1,6 +1,8 @@
 import {
-  StyleSheet, Text, View, Button,
+  View, KeyboardAvoidingView, TextInput, Image, Text, TouchableOpacity,
 } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
+import Btn from '../component/Btn';
 
 function Register(props) {
   const { navigation } = props;
@@ -17,21 +19,40 @@ function Register(props) {
   };
 
   return (
-    <View style={styles.container}>
-      <Text>REGISTER SCREEN</Text>
-      <Button title="Register" onPress={() => press.createAccount()} />
-      <Button title="Login" onPress={() => press.toLoginScreen()} />
+    <View className="flex-1 bg-white py-14 px-7 items-center">
+      <Image className="w-44 h-44 mt-14" source={require('../assets/finally.png')} />
+      <KeyboardAvoidingView className="z-20 w-full relative justify-center self-center">
+        <View className="items-start px-8 bg-white rounded-xl py-5 shadow-2xl">
+          <View className="flex-row rounded-md border border-slate-700 p-3 w-full items-center my-5">
+            <FontAwesome5 name="user-alt" size={20} color="#FCAF39" />
+            <TextInput className="pl-4 placeholder:text-xs placeholder:text-black" placeholder="Name" />
+          </View>
+
+          <View className="flex-row rounded-md border border-slate-700 p-3 w-full items-center my-5">
+            <FontAwesome5 name="user-astronaut" size={20} color="#FCAF39" />
+            <TextInput className="pl-4 placeholder:text-xs placeholder:text-black" placeholder="Username" />
+          </View>
+
+          <View className="flex-row rounded-md border border-slate-700 p-3 w-full items-center my-5">
+            <FontAwesome5 name="lock" size={20} color="#FCAF39" />
+            <TextInput className="pl-4 placeholder:text-xs placeholder:text-black" placeholder="Masukkan Tanggal Keberangkatan" />
+          </View>
+
+          <View className="w-full py-6">
+            <Btn text="Sign Up" pindah={press.createAccount} />
+          </View>
+        </View>
+      </KeyboardAvoidingView>
+      <View className="flex-row">
+        <Text>sudah memiliki akun? </Text>
+        <TouchableOpacity onPress={() => press.toLoginScreen()}>
+          <Text className="text-blue-600">
+            Login
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default Register;
