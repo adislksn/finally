951d7f514648935063ead1516fbcbeeb9596e5d0
adislksn <adi.120140038@student.itera.ac.id>
2022-12-01 17:06:36 +0700

    Slider Button

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,12 +1,14 @@
 import {
   StyleSheet, Text, View, Image, StatusBar, TouchableOpacity
 } from 'react-native';
+import React, { useState } from 'react';
 import { MaterialIcons } from '@expo/vector-icons';
-import { Slider } from 'react-input-slider';
+import Slider from '@react-native-community/slider';
 import Btn from '../component/Btn';
 
 function Home(props) {
   const { navigation } = props;
+  const [range, setRange] = useState(0);
 
   // Handle press events.
   const press = {
@@ -38,15 +40,24 @@ function Home(props) {
         <View className="rounded-full border-orange-400 border-2 p-3">
           <View className="rounded-full border-orange-400 border-4 p-3 w-max">
             <View className="rounded-full border-orange-400 border-8 w-36 h-36 p-3 justify-center items-center">
-              <Text className="text-xl">20 KM</Text>
+              <Text className="text-xl">{range} KM</Text>
             </View>
           </View>
         </View>
       </View>
+
+      <View className="w-full py-10">
+        <Slider className="w-11/12 h-10" 
+                thumbTintColor="#FB4911" 
+                minimumValue={0} step={5}
+                maximumValue={100} 
+                minimumTrackTintColor="#FCAF39" maximumTrackTintColor="#D9D9D9" 
+                value={30} onValueChange={setRange}/>
+      </View>
       <View>
-      <Btn text="Profile" pindah={press.toProfile}/>
-      <Btn text="Cari Teman" pindah={press.toMaps} />
-      <Btn text="Keluar" pindah={press.logout} />
+        <Btn text="Profile" pindah={press.toProfile}/>
+        <Btn text="Cari Teman" pindah={press.toMaps} />
+        <Btn text="Keluar" pindah={press.logout} />
       </View>
     </View>
   );
