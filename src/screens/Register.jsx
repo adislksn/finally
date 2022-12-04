456aa9419ace95d311aa39a839e57d2dea46fe3e
adislksn <adi.120140038@student.itera.ac.id>
2022-12-04 22:57:02 +0700

    Behaviour Keyboardavoidingview

--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -1,5 +1,5 @@
 import {
-  View, KeyboardAvoidingView, TextInput, Image, Text, TouchableOpacity, StyleSheet, ScrollView,
+  View, KeyboardAvoidingView, TextInput, Image, Text, TouchableOpacity, StyleSheet, Platform,
 } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 import Btn from './_components/Btn';
@@ -20,52 +20,50 @@ function Register(props) {
 
   return (
     <View className="flex-1 bg-white">
-      <ScrollView>
-        <View className="flex-1 bg-white pt-3 px-2 items-center">
+      <View className="flex-1 bg-white pt-3 px-2 items-center">
 
-          <Image className="w-24 h-24 mt-14" source={require('../assets/finally.png')} />
+        <Image className="w-24 h-24 mt-14" source={require('../assets/finally.png')} />
 
-          <Text className="text-4xl mt-16" style={style.textTitle}>Register</Text>
+        <Text className="text-4xl mt-16" style={style.textTitle}>Register</Text>
 
-          <KeyboardAvoidingView className="z-20 w-full justify-center self-center">
-            <View className="items-start px-8 bg-white rounded-xl py-5 shadow-2xl">
+        <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} className="z-20 w-full justify-center self-center">
+          <View className="items-start px-8 bg-white rounded-xl py-5 shadow-2xl">
 
-              <View className="flex-row rounded-md p-3 w-full items-center my-4" style={style.inputWrapper}>
-                <FontAwesome5 name="user-alt" size={17} style={style.inputIcon} />
-                <TextInput className="w-10/12 pl-4 placeholder:text-s placeholder:text-black" placeholder="Name" />
-              </View>
-
-              <View className="flex-row rounded-md p-3 w-full items-center my-4" style={style.inputWrapper}>
-                <FontAwesome5 name="user-astronaut" size={17} style={style.inputIcon} />
-                <TextInput className="w-10/12 pl-4 placeholder:text-s placeholder:text-black" placeholder="Username" />
-              </View>
-
-              <View className="flex-row rounded-md p-3 w-full items-center my-4" style={style.inputWrapper}>
-                <FontAwesome5 name="lock" size={17} style={style.inputIcon} />
-                <TextInput secureTextEntry className="w-10/12 pl-4 placeholder:text-s placeholder:text-black" placeholder="Password" />
-              </View>
-
-              <View className="flex-row rounded-md p-3 w-full items-center my-4" style={style.inputWrapper}>
-                <FontAwesome5 name="lock" size={17} style={style.inputIcon} />
-                <TextInput secureTextEntry className="w-10/12 pl-4 placeholder:text-s placeholder:text-black" placeholder="Repeat Password" />
-              </View>
-
-              <View className="w-full">
-                <Btn text="Sign Up" pindah={press.createAccount} />
-              </View>
-
+            <View className="flex-row rounded-md p-3 w-full items-center my-4 bg-gray-100">
+              <FontAwesome5 name="user-alt" size={17} style={style.inputIcon} />
+              <TextInput className="w-10/12 pl-4 placeholder:text-s placeholder:text-black" placeholder="Name" />
             </View>
-          </KeyboardAvoidingView>
 
-          <View className="flex-row mb-5">
-            <Text>Sudah memiliki akun? </Text>
-            <TouchableOpacity onPress={() => press.toLoginScreen()}>
-              <Text className="text-blue-600">Login</Text>
-            </TouchableOpacity>
+            <View className="flex-row rounded-md p-3 w-full items-center my-4 bg-gray-100">
+              <FontAwesome5 name="user-astronaut" size={17} style={style.inputIcon} />
+              <TextInput className="w-10/12 pl-4 placeholder:text-s placeholder:text-black" placeholder="Username" />
+            </View>
+
+            <View className="flex-row rounded-md p-3 w-full items-center my-4 bg-gray-100">
+              <FontAwesome5 name="lock" size={17} style={style.inputIcon} />
+              <TextInput secureTextEntry className="w-10/12 pl-4 placeholder:text-s placeholder:text-black" placeholder="Password" />
+            </View>
+
+            <View className="flex-row rounded-md p-3 w-full items-center my-4 bg-gray-100">
+              <FontAwesome5 name="lock" size={17} style={style.inputIcon} />
+              <TextInput secureTextEntry className="w-10/12 pl-4 placeholder:text-s placeholder:text-black" placeholder="Repeat Password" />
+            </View>
+
+            <View className="w-full">
+              <Btn text="Sign Up" pindah={press.createAccount} />
+            </View>
+
           </View>
+        </KeyboardAvoidingView>
 
+        <View className="flex-row mb-5">
+          <Text>Sudah memiliki akun? </Text>
+          <TouchableOpacity onPress={() => press.toLoginScreen()}>
+            <Text className="text-blue-600">Login</Text>
+          </TouchableOpacity>
         </View>
-      </ScrollView>
+
+      </View>
     </View>
   );
 }
@@ -73,9 +71,6 @@ function Register(props) {
 const style = StyleSheet.create({
   textTitle: {
     fontFamily: 'Urbanist-Bold',
-  },
-  inputWrapper: {
-    backgroundColor: '#F1F1F1',
   },
   inputIcon: {
     color: '#C5C5C7',
