2aafb13134d75930994c25ed976a22b390336198
adislksn <adi.120140038@student.itera.ac.id>
2022-12-13 13:20:29 +0700

    Tambah marker Maps

--- src/screens/Maps/index.jsx
+++ src/screens/Maps/index.jsx
@@ -1,5 +1,5 @@
 import { SafeAreaView } from 'react-native';
-import MapView from 'react-native-maps';
+import MapView, { Marker } from 'react-native-maps';
 
 import FindingBox from './FindingBox';
 import Header from '../_components/Header';
@@ -20,7 +20,24 @@ function Maps(props) {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
-      />
+      >
+        <Marker
+          coordinate={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+          }}
+          title="Titik satu"
+          description="Air panas-air panas"
+        />
+        <Marker
+          coordinate={{
+            latitude: 37.76655,
+            longitude: -122.4324,
+          }}
+          title="Titik kedua"
+          description="Air dingin-air dingin"
+        />
+      </MapView>
 
       <FindingBox navigation={navigation} />
 
