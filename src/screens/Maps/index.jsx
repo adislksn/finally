6071c5768b981aa2601df9b7b75a8568bac0a459
adislksn <adi.120140038@student.itera.ac.id>
2022-12-18 23:08:36 +0700

    Flatlist Map

--- src/screens/Maps/index.jsx
+++ src/screens/Maps/index.jsx
@@ -1,14 +1,39 @@
-import { SafeAreaView } from 'react-native';
+import { View, FlatList } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
-
 import FindingBox from './FindingBox';
 import Header from '../_components/Header';
+// import {} from '../redux/features/user';
+
+// const url = '/api/friends';
+// const body = user.friends;
 
 function Maps(props) {
+  const markerLocation = [
+    {
+      id: 0,
+      latitude: 37.78825,
+      longitude: -122.4324,
+      tittle: 'nama',
+      description: 'bio',
+    },
+    {
+      id: 1,
+      latitude: 37.76655,
+      longitude: -122.4324,
+      tittle: 'nama',
+      description: 'bio',
+    },
+    {
+      id: 2,
+      latitude: 37.78888,
+      longitude: -122.4324,
+      tittle: 'nama',
+      description: 'bio',
+    },
+  ];
   const { navigation } = props;
-
   return (
-    <SafeAreaView className="flex-1 bg-white">
+    <View className="flex-1 bg-white">
 
       <Header title="Mencari Teman" backTo="Home" navigation={navigation} />
 
@@ -21,27 +46,27 @@ function Maps(props) {
           longitudeDelta: 0.0421,
         }}
       >
-        <Marker
-          coordinate={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-          }}
-          title="Titik satu"
-          description="Air panas-air panas"
+        <FlatList
+          data={markerLocation}
+          renderItem={({ item }) => (
+            <Marker
+              coordinate={{ latitude: item.latitude, longitude: item.longitude }}
+              title={item.tittle}
+              description={item.description}
+            />
+          )}
+          keyExtractor={(item) => item.id}
         />
         <Marker
-          coordinate={{
-            latitude: 37.76655,
-            longitude: -122.4324,
-          }}
-          title="Titik kedua"
-          description="Air dingin-air dingin"
+          coordinate={{ latitude: 37.76888, longitude: -122.4324 }}
+          title="test"
+          description="oke"
         />
       </MapView>
 
       <FindingBox navigation={navigation} />
 
-    </SafeAreaView>
+    </View>
   );
 }
 
