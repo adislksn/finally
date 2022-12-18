f184402b6c3f528a4283cc600851297fabff3b23
Adli Mustofa <103428648+AdliMustofa120140022@users.noreply.github.com>
2022-12-18 20:28:42 +0700

    modal demo

--- src/screens/Demo.jsx
+++ src/screens/Demo.jsx
@@ -13,6 +13,26 @@ export default function Demo(props) {
   };
   return (
     <View className="flex-1 justify-center items-center">
+      <StatusBar />
+      <Modal
+        animationType="slide"
+        transparent
+        visible={showModal}
+      >
+        <View className="flex-1 justify-center items-center" style={{ backgroundColor: 'rgba(52, 52, 52, 0.7)' }}>
+          <View>
+            <Text className="text-white">Hello World!</Text>
+          </View>
+          <TouchableOpacity onPress={() => toggleModal()}>
+            <Text className="text-white">Toggle Modal</Text>
+          </TouchableOpacity>
+        </View>
+      </Modal>
+      <TouchableOpacity className="bg-slate-500" onPress={() => { toggleModal(); }}>
+        <Text style={{ color: 'red', fontSize: 20, fontWeight: 'bold' }}>
+          Hello world
+        </Text>
+      </TouchableOpacity>
       
     </View>
   );
